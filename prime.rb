63c4c6edc819feba4c4d0aa12cf8bb940f99548e
Joel Hobgood <jhobgoo@gmail.com>
2020-02-21 07:02:08 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,9 @@
 def prime?(integer)
-  if integer < 2 || =< 0
-    false    
-  else
-    true
-  end
+  return false if integer < 2
+  (2..integer - 1).each do |x|
+    if (integer % x) == 0
+      false
+    else
+      true
+    end
 end
